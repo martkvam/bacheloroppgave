db1df6bd2684dce675cc1beac9bc78f7344855a7
martkvam <56063587+martkvam@users.noreply.github.com>
2022-02-15 10:04:42 +0100

    Setup

---
+++ case-service/src/main/java/com/finbev/services/caseservice/CaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finbev.services.caseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseServiceApplication.class, args);
+	}
+
+}
