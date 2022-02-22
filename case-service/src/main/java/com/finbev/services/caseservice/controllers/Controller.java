edf1281629e9d465cec9bc160a8464262fa39e11
martkvam <56063587+martkvam@users.noreply.github.com>
2022-02-22 11:44:48 +0100

    Lagt inn cors

--- case-service/src/main/java/com/finbev/services/caseservice/controllers/Controller.java
+++ case-service/src/main/java/com/finbev/services/caseservice/controllers/Controller.java
@@ -1,6 +1,7 @@
 package com.finbev.services.caseservice.controllers;
 
 import com.finbev.services.caseservice.models.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 public class Controller {
 
     @GetMapping("/case")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Case> getCases(){
        return opprettListe();
     }
