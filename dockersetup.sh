7a9605ae0e602dff049dc694d319efb47ba44da9
martkvam <56063587+martkvam@users.noreply.github.com>
2022-03-30 13:10:18 +0200

    Endring i docker-compose.yml

--- dockersetup.sh
+++ dockersetup.sh
@@ -2,19 +2,19 @@
 
 echo "TEST"
 cd \brokerservice
-docker build -t martkvam/bachelor-brokerservice:0.0.1-SNAPSHOT .
+docker build -t springio/bachelor-brokerservice:0.0.1-SNAPSHOT .
 
 cd ../api-gateway
-docker build -t martkvam/bachelor-api-gateway:0.0.1-SNAPSHOT .
+docker build -t springio/bachelor-api-gateway:0.0.1-SNAPSHOT .
 
 cd ../altinn-service
-docker build -t martkvam/bachelor-altinn-service:0.0.1-SNAPSHOT .
+docker build -t springio/bachelor-altinn-service:0.0.1-SNAPSHOT .
 
 cd ../case-service
-docker build -t martkvam/bachelor-case-service:0.0.1-SNAPSHOT .
+docker build -t springio/bachelor-case-service:0.0.1-SNAPSHOT .
 
 cd ../naming-server
-docker build -t martkvam/bachelor-naming-server:0.0.1-SNAPSHOT .
+docker build -t springio/bachelor-naming-server:0.0.1-SNAPSHOT .
 
 cd ../docker
 docker compose up
