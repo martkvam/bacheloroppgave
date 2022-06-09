211eba6ded166068bd15cf5a81ae6463fb9edb62
adnilsen <adrian.bn@hotmail.com>
2022-06-09 20:52:28 +0200

    Endringer

--- brokerservice/src/main/resources/data.sql
+++ brokerservice/src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into broker(id, broker_name)
 values (1, 'Krogsveen'),
        (2, 'Privatmegleren'),
        (3, 'Aktiv Eiendomsmegling'),
-       (4, 'DnB Eiendom'),
+       (4, 'DNB Eiendom'),
        (5, 'Eiendomsmegler 1'),
        (6, 'Eiendomsmegler Vest'),
        (7, 'Nordvik');
